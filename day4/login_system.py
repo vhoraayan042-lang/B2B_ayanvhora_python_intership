2ebcfa880114a7f105ea8cc2cd12f8020576d4a7
vhoraayesha305-afk <vhoraayesha305@gmail.com>
2026-05-21 22:59:57 +0530

    first commit

---
+++ day4/login_system.py
@@ -0,0 +1,11 @@
+
+CORRECT_USERNAME = "ayan"
+CORRECT_PASSWORD = "251106"
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == CORRECT_USERNAME and password == CORRECT_PASSWORD:
+  print("Login Successful")
+else:
+  print("Login Failed")
